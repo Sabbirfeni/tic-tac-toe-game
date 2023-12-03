d2561db6b4cac1457de271ec8541b1c52fd0f23b
sabbir <contact.sabbirbhuiyan@gmail.com>
2023-12-03 23:19:46 +0600

    player slider designed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 // Declaring html elements
 const selectBox = document.querySelector('.select-box'),
 selectXBtn = selectBox.querySelector('.playerX'),
-selectOBtn = selectBox.querySelector('.player0'),
-playBoard = document.querySelector('.play-board');
-
+selectOBtn = selectBox.querySelector('.playerO'),
+playBoard = document.querySelector('.play-board'),
+allBox = document.querySelectorAll('section span'),
+players = document.querySelector('.players')
 window.onload = () => {
+    allBox.forEach(box => {
+        box.setAttribute('onclick', 'clickedBox(this)')
+    })
     selectXBtn.onclick = () => {
         selectBox.classList.add('hide')
         playBoard.classList.add('show')
+    }
+    selectOBtn.onclick = () => {
+        selectBox.classList.add('hide')
+        playBoard.classList.add('show')
+        players.setAttribute('class', 'players active')
     }
 }
